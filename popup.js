d5400511e4541d5e41dcfe4610ab45a55b730ac8
oytundd <42172759+oytundd@users.noreply.github.com>
2019-04-09 06:06:55 +0300

    New Functionality
    Extension now toggles the guilds tab too.

--- popup.js
+++ popup.js
@@ -14,42 +14,47 @@ changeColor.onclick = function(element)
             var channels = document.getElementsByClassName("channels-Ie2l6A");
             var guilds   = document.getElementsByClassName("wrapper-1Rf91z");
 
-            if (channels[0].style.width == "240px")
-              {
-                console.log("Condition 1");
-                for (var i = 0; i < channels.length; i++) 
-                  {
-                    channels[i].style.width = "45px";
-                  }
-              }
-            else if(channels[0].style.width == "45px")
-              {
-                for (var i = 0; i < channels.length; i++) 
-                  {
-                    channels[i].style.width = "0px";
-                  }
-              }
-            else if(channels[0].style.width == "0px" && guilds[0].style.width == "70px")
-              {
-                for (var i = 0; i < channels.length; i++) 
-                  {
-                    channels[i].style.width ="0px";
-                  }
-                for (var i = 0; i < guilds.length; i++) 
-                  {
-                    guilds[i].style.width ="0px";
-                  }
-              }
-            else if(channels[0].style.width == "0px" && guilds[0].style.width == "0px")
-                for (var i = 0; i < channels.length; i++) 
-                  {
-                    channels[i].style.width = "240px";
-                  }
-                for (var i = 0; i < guilds.length; i++) 
-                  {
-                    guilds[i].style.width = "70px";
-                  }
-          `
+            if (channels[0].style.width == "240px" && guilds[0].style.width == "70px") {
+              for (var i = 0; i < channels.length; i++) 
+                {
+                  channels[i].style.width = "45px";
+                }
+            } else if(channels[0].style.width == "45px" && guilds[0].style.width == "70px") {
+              for (var i = 0; i < channels.length; i++) 
+                {
+                  channels[i].style.width = "0px";
+                }
+            } else if(channels[0].style.width == "0px" && guilds[0].style.width == "70px") {
+              for (var i = 0; i < channels.length; i++) 
+                {
+                  channels[i].style.width ="0px";
+                }
+              for (var i = 0; i < guilds.length; i++) 
+                {
+                  guilds[i].style.width ="0px";
+                }
+            }
+          else if(channels[0].style.width == "0px" && guilds[0].style.width == "0px") {
+              for (var i = 0; i < channels.length; i++) 
+                {
+                  channels[i].style.width = "45px";
+                }
+              for (var i = 0; i < guilds.length; i++) 
+                {
+                  guilds[i].style.width = "0px";
+                }
+          }
+          else {
+            for (var i = 0; i < channels.length; i++) 
+            {
+              channels[i].style.width = "240px";
+            }
+            for (var i = 0; i < guilds.length; i++) 
+            {
+              guilds[i].style.width = "70px";
+            }
+          }
+            `
           }
       )
     }
--- test.js
+++ test.js
@@ -1,16 +1,52 @@
+var channels = document.getElementsByClassName("channels-Ie2l6A");
+var guilds   = document.getElementsByClassName("wrapper-1Rf91z");
 
-         var elements = document.getElementsByClassName("channels-Ie2l6A");
-         if (elements[0].style.width ="240px")
-         {
-         for (var i = 0; i < elements.length; i++) 
-         {
-             elements[i].style.width ="0px";
-         }
-         }
-         else 
-         {
-         for (var i = 0; i < elements.length; i++) 
-         {
-             elements[i].style.width ="240px";
-         }
-        }
+if (channels[0].style.width == "240px" && guilds[0].style.width == "70px")
+{
+  console.log("Condition 1");
+  for (var i = 0; i < channels.length; i++) 
+    {
+      channels[i].style.width = "45px";
+    }
+}
+else if(channels[0].style.width == "45px" && guilds[0].style.width == "70px")
+{
+  for (var i = 0; i < channels.length; i++) 
+    {
+      channels[i].style.width = "0px";
+    }
+}
+else if(channels[0].style.width == "0px" && guilds[0].style.width == "70px")
+{
+  for (var i = 0; i < channels.length; i++) 
+    {
+      channels[i].style.width ="0px";
+    }
+  for (var i = 0; i < guilds.length; i++) 
+    {
+      guilds[i].style.width ="0px";
+    }
+}
+else if(channels[0].style.width == "0px" && guilds[0].style.width == "0px")
+{
+  for (var i = 0; i < channels.length; i++) 
+    {
+      channels[i].style.width = "45px";
+    }
+  for (var i = 0; i < guilds.length; i++) 
+    {
+      guilds[i].style.width = "0px";
+    }
+}
+else if(channels[0].style.width == "45px" && guilds[0].style.width == "0px")
+{
+for (var i = 0; i < channels.length; i++) 
+{
+  channels[i].style.width = "240px";
+}
+for (var i = 0; i < guilds.length; i++) 
+{
+  guilds[i].style.width = "70px";
+}
+
+}
