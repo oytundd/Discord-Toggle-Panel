7586e957043fe3443bef205ccf5cf7c65d9dc04d
oytundd <42172759+oytundd@users.noreply.github.com>
2019-04-09 05:30:31 +0300

    More functionality.
    Started working on more functionality.

--- popup.js
+++ popup.js
@@ -1,27 +1,57 @@
-changeColor.onclick = function(element) {
+changeColor.onclick = function(element) 
+{
   let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code:
-         `
-          var elements = document.getElementsByClassName("channels-Ie2l6A");
-          if (elements[0].style.width =="240px")
+  chrome.tabs.query
+  (
+      {active: true, currentWindow: true}, function(tabs) 
+    {
+      chrome.tabs.executeScript
+      (
+          tabs[0].id,
           {
-          for (var i = 0; i < elements.length; i++) 
-          {
-              elements[i].style.width ="45px";
+            code:
+          `
+            var channels = document.getElementsByClassName("channels-Ie2l6A");
+            var guilds   = document.getElementsByClassName("wrapper-1Rf91z");
+
+            if (channels[0].style.width == "240px")
+              {
+                console.log("Condition 1");
+                for (var i = 0; i < channels.length; i++) 
+                  {
+                    channels[i].style.width = "45px";
+                  }
+              }
+            else if(channels[0].style.width == "45px")
+              {
+                for (var i = 0; i < channels.length; i++) 
+                  {
+                    channels[i].style.width = "0px";
+                  }
+              }
+            else if(channels[0].style.width == "0px" && guilds[0].style.width == "70px")
+              {
+                for (var i = 0; i < channels.length; i++) 
+                  {
+                    channels[i].style.width ="0px";
+                  }
+                for (var i = 0; i < guilds.length; i++) 
+                  {
+                    guilds[i].style.width ="0px";
+                  }
+              }
+            else if(channels[0].style.width == "0px" && guilds[0].style.width == "0px")
+                for (var i = 0; i < channels.length; i++) 
+                  {
+                    channels[i].style.width = "240px";
+                  }
+                for (var i = 0; i < guilds.length; i++) 
+                  {
+                    guilds[i].style.width = "70px";
+                  }
+          `
           }
-          }
-          else 
-          {
-          for (var i = 0; i < elements.length; i++) 
-          {
-              elements[i].style.width ="240px";
-          }
-          }`
-        }
-    )
-  }
+      )
+    }
   )
-  }
+}
